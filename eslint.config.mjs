82592b8b00f5f164debb237af5fc1a3d9df4de91
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-07-29 20:47:20 +0530

    chore: improved the eslint rules to support prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
-export default withNuxt({
-  rules: {
-    'vue/html-self-closing': 'off',
+export default withNuxt(
+  {
+    rules: {
+      'vue/html-self-closing': 'off',
+    },
   },
-});
+  eslintConfigPrettier,
+);
