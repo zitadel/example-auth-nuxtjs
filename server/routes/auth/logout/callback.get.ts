a4184bba1713cdb5cf599c520ae0a2d9cba31fee
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2026-05-07 14:31:09 +0300

    fix: set cookie path on logout_state deletion

--- server/routes/auth/logout/callback.get.ts
+++ server/routes/auth/logout/callback.get.ts
@@ -26,7 +26,7 @@ export default defineEventHandler(async (event: H3Event) => {
         deleteCookie(event, name);
       }
     }
-    deleteCookie(event, 'logout_state');
+    deleteCookie(event, 'logout_state', { path: '/auth/logout/callback' });
 
     const successUrl = new URL('/logout/success', getRequestURL(event));
     return sendRedirect(event, successUrl.toString());
